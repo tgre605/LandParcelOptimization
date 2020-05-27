2f0b85d1e905125b19f501abf9f756d9cac01d9e
Aarone Koshy <aarone.k.496@gmail.com>
2020-05-27 12:59:51 +1200

    Setting up json reading project

---
+++ src/main/java/JsonReader.java
@@ -0,0 +1,15 @@
+/*
+    Class for reading json output of road network
+
+    Should probably use org.json for reading
+ */
+import org.json.*;
+
+
+public class JsonReader {
+
+    public JsonReader(String file){
+        System.out.println(file);
+
+    }
+}
---
+++ src/main/java/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+
+    public static void main(String[] args) {
+        JsonReader reader = new JsonReader("/input/roadnetwork.json");
+    }
+}

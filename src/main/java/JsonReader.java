6d7c71fb22aeb0fcde0025f466ebdf53567e726a
tgre605 <48343370+tgre605@users.noreply.github.com>
2020-05-27 15:00:48 +1200

    Read JSON input file and output x and z coordinates of verticies

--- src/main/java/JsonReader.java
+++ src/main/java/JsonReader.java
@@ -4,12 +4,42 @@
     Should probably use org.json for reading
  */
 import org.json.*;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
 
+import java.io.FileReader;
+import java.io.IOException;
 
 public class JsonReader {
 
     public JsonReader(String file){
-        System.out.println(file);
 
+        JSONParser jsonParser = new JSONParser();
+        try (FileReader reader = new FileReader(file)){
+            Object obj = jsonParser.parse(reader);
+
+            JSONObject parcels = (JSONObject) obj;
+            JSONArray land_usages = (JSONArray) parcels.get("land_usages");
+
+            land_usages.forEach(land -> parsePolygons((JSONObject) land));
+        } catch (ParseException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static void parsePolygons (JSONObject landUsage){
+        JSONArray polygon = (JSONArray) landUsage.get("polygon");
+        polygon.forEach(line -> parseVertices((JSONObject) line));
+    }
+
+    private static void parseVertices (JSONObject polygon){
+        double xPoint = (double) polygon.get("x");
+        System.out.println("x: "+xPoint);
+        double zPoint = (double) polygon.get("z");
+        System.out.println("z: "+zPoint);
     }
 }
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,6 @@
 public class Main {
 
     public static void main(String[] args) {
-        JsonReader reader = new JsonReader("/input/roadnetwork.json");
+        JsonReader reader = new JsonReader("/LandParcelOptimization/input/roadnetwork.json");
     }
 }

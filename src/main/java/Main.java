a32ccff53c8979d2a0b4a00077b2d3dbd0ca6ad3
Aarone Koshy <aarone.k.496@gmail.com>
2020-06-03 14:44:11 +1200

    Added zoom functionality still slow

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,10 +1,14 @@
 import javafx.application.Application;
+import javafx.event.EventHandler;
 import javafx.scene.Group;
 import javafx.scene.Scene;
+import javafx.scene.input.ScrollEvent;
+import javafx.scene.layout.Pane;
 import javafx.scene.shape.Polygon;
 import javafx.stage.Stage;
 import javafx.scene.paint.Color;
 
+import java.awt.*;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ArrayList;
@@ -22,7 +26,7 @@ public class Main extends Application {
         landParcels = reader.getParcels();
 
         //Creating a Group object
-        Group root = new Group();
+        Pane root = new Pane();
 
         ArrayList<Color> colorList = new ArrayList<Color>();
         colorList.add(Color.BLUE);
@@ -41,7 +45,21 @@ public class Main extends Application {
         }
 
         //Creating a scene object
-        Scene scene = new Scene(root, 600, 300);
+        Scene scene = new Scene(root, 900, 900);
+
+        scene.setOnScroll(new EventHandler<ScrollEvent>() {
+            @Override
+            public void handle(ScrollEvent event) {
+                double scale = root.getScaleX();
+                if (event.getDeltaY() > 0)
+                    scale /= 1.2;
+                else
+                    scale *= 1.2;
+                root.setScaleX(scale);
+                root.setScaleY(scale);
+            }
+        });
+
 
         //Setting title to the Stage
         stage.setTitle("Drawing a Polygon");

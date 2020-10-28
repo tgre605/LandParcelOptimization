f822a5b1e5a858d2fd62b129fa55c981a63665d4
tgre605 <tgre605@aucklanduni.ac.nz>
2020-10-28 13:30:30 +1300

    testing

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -34,11 +34,11 @@ public class Main extends Application {
         int i = 0;
         long startTime = System.currentTimeMillis();
         System.out.println(LandParcels.size());
-        int parcelCount = 100;
+        int parcelCount = 1169;
         for (LandParcel parcel : LandParcels) {
             if (i != 110 && i != 247 && i != 287 && i != 299 && i != 427 && i != 439 && i != 954 && i != 1010 && i != 1169) {
                 parcel.surroundingParcels(reader);
-                switch (parcel.landType){
+                switch (parcel.landType) {
                     case residential:
                         boundingBoxOptimizer.BoundingBoxOptimization(parcel, 5, 5, 1.5);
                         break;
@@ -65,47 +65,49 @@ public class Main extends Application {
         i = 0;
         startTime = System.currentTimeMillis();
         for (LandParcel parcel : LandParcels) {
+            if (i != 370 && i != 790 && i != 526) {
+                reader.getBuildingFootprints(currentDir.toAbsolutePath() + "/input/buildingFootprints.json");
+                placer.setRoadCentre(parcel);
+                placer.surroundingFootprints(parcel);
+                placer.placeBuildings(parcel, reader);
 
-            reader.getBuildingFootprints(currentDir.toAbsolutePath() + "/input/buildingFootprints.json");
-            placer.setRoadCentre(parcel);
-            placer.surroundingFootprints(parcel);
-            placer.placeBuildings(parcel, reader);
-
-            switch (parcel.landType) {
-                case industry:
-                    SceneRenderer.render(parcel.getFootprintGeometries(), SceneRenderer.ColorSpectrum.Yellow);
-                    break;
-                case commercial:
-                    SceneRenderer.render(parcel.getFootprintGeometries(), SceneRenderer.ColorSpectrum.Blue);
-                    break;
-                case residential:
-                    SceneRenderer.render(parcel.getFootprintGeometries(), SceneRenderer.ColorSpectrum.Green);
-                    break;
-                case undefined:
-                    SceneRenderer.render(parcel.getFootprintGeometries(), SceneRenderer.ColorSpectrum.Red);
-                    break;
-            }
-
-            for (Footprint footprint : parcel.footprints) {
-                //if (footprint.id == 157 || footprint.id == 165) {
-                //    placer.createDriveway(footprint);
-                //}
-
-                if (footprint.building != null) {
-                    //SceneRenderer.render(footprint.building.polygon, Color.GREY);
+                switch (parcel.landType) {
+                    case industry:
+                        SceneRenderer.render(parcel.getFootprintGeometries(), SceneRenderer.ColorSpectrum.Yellow);
+                        break;
+                    case commercial:
+                        SceneRenderer.render(parcel.getFootprintGeometries(), SceneRenderer.ColorSpectrum.Blue);
+                        break;
+                    case residential:
+                        SceneRenderer.render(parcel.getFootprintGeometries(), SceneRenderer.ColorSpectrum.Green);
+                        break;
+                    case undefined:
+                        SceneRenderer.render(parcel.getFootprintGeometries(), SceneRenderer.ColorSpectrum.Red);
+                        break;
                 }
 
-            }
+                for (Footprint footprint : parcel.footprints) {
+                    //if (footprint.id == 157 || footprint.id == 165) {
+                    //    placer.createDriveway(footprint);
+                    //}
 
-            i++;
+                    if (footprint.building != null) {
+                        SceneRenderer.render(footprint.building.polygon, Color.GREY);
+                    }
 
-            if(i == parcelCount){
-                break;
+                }
+
+                i++;
+                
+                if (i == parcelCount) {
+                    break;
+                }
+                System.out.println("Buildings placed: " + i + " in " + (System.currentTimeMillis() - startTime) / 1000 + "s");
             }
-            System.out.println("Buildings placed: " + i + " in " + (System.currentTimeMillis() - startTime) / 1000 + "s");
         }
         System.out.println("Finished Computing");
         sceneRenderer.start(stage);
+
     }
 
     public static void main(String[] args) {
